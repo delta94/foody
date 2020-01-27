96ba37bdfd9418ce9fece4fee5e1861f699fccd4
Stéphane <stephane@novaway.fr>
2020-01-27 07:30:48 +0100

    :lipstick: Updating input UI

--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 // @ts-ignore
 import { TextInput, StyleSheet } from 'react-native';
 
-export const Input: React.FC = () => <TextInput style={styles.input} />;
+interface Props {
+  customStyle?: {
+    [key: string]: any;
+  };
+}
+
+export const Input: React.FC<Props> = ({ customStyle }) => (
+  <TextInput style={[styles.input, customStyle]} />
+);
 
 const styles = StyleSheet.create({
   input: {
     width: '100%',
-    height: 70,
+    height: 60,
     backgroundColor: 'white',
-    fontSize: 18,
+    fontSize: 16,
     borderRadius: 100,
     shadowColor: 'rgba(0, 0, 0, .1)',
     shadowOffset: { width: 0, height: 28 },
     shadowOpacity: 0.8,
     shadowRadius: 43,
-    paddingHorizontal: 35,
+    paddingHorizontal: 20,
   },
 });
