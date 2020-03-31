5630ed7f84774eda54290e2148b4d57396a0283f
Stéphane <contact@stephane-richin.fr>
2020-03-31 08:55:27 +0200

    :bento: Adding default icon

--- packages/ui/src/components/Footer/index.web.tsx
+++ packages/ui/src/components/Footer/index.web.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Text } from '../Text';
 import { Spacer } from '../Spacer';
 import { Row } from '../Grid/Row';
+import { useMediaQuery } from '../../hooks';
 
 const CURRENT_DATE = new Date().getUTCFullYear();
 
-export const Footer: React.FC = () => (
-  <Row gutter>
-    <Text>{CURRENT_DATE} © Foody</Text>
-    <Spacer height={60} />
-  </Row>
-);
+export const Footer: React.FC = () => {
+  const { isDesktop } = useMediaQuery();
+
+  return (
+    <Row gutter>
+      <Text>{CURRENT_DATE} © Foody</Text>
+      <Spacer height={isDesktop ? 60 : 20} />
+    </Row>
+  );
+};
