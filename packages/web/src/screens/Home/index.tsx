18809358aa9464bdbf348608d23c15b19faa31a0
Stéphane <contact@stephane-richin.fr>
2020-03-31 17:58:54 +0200

    :speech_balloon: Updating home text

--- packages/web/src/screens/Home/index.tsx
+++ packages/web/src/screens/Home/index.tsx
@@ -1,18 +1,26 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import {
   Main,
   Title,
   Text,
+  Spacer
   // @ts-ignore
 } from '@foody/ui';
 
 const Home = () => (
   <Main gutter>
-    <Title title="Home" />
+    <Title title="Trouver une recette" />
     <Text>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, sapiente impedit maiores
-      aspernatur culpa, vero amet accusamus corporis fugit nesciunt, nostrum nemo? Maxime corporis
-      temporibus ducimus doloremque nemo dolorum esse.
+      Envie d'une recette avec des ingrédiants en particulier ? Détéctez vos
+      aliments à partir d'une photo et trouver la recette qui vous donne envi.
+    </Text>
+    <Spacer height={20} />
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quod quis
+      minima optio, rem beatae sed incidunt vitae voluptates neque provident,
+      praesentium quas maxime quisquam dolore corrupti repellendus, facere
+      ducimus?
     </Text>
   </Main>
 );
